f0cb0e803cadcec84bf330409b26c37d853e23c1
Mick <47728875+iDevMC@users.noreply.github.com>
2020-11-03 23:13:14 -0500

    🍂 Discord.java

---
+++ src/java/network/starplum/Discord.java
@@ -0,0 +1,14 @@
+package network.starplum;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Discord extends JavaPlugin {
+
+	@Override public void onLoad() {}
+	@Override public void onEnable() { /** nothing needed really lol */}
+	@Override public void onDisable() {}
+	@Override public String toString() { return ""; }
+	@Override protected Object clone() throws CloneNotSupportedException { return this; }
+	@Override protected void finalize() throws Throwable {}
+	
+}
